16ff9ced38e1e12aa80900b135b80741975ede6c
Arjun <nagarjun2790@gmail.com>
2026-06-22 00:13:22 +0530

    fix: disabled form-login and http-basic

--- analytics-service/src/main/java/com/shortlyai/analytics/common/security/SecurityConfig.java
+++ analytics-service/src/main/java/com/shortlyai/analytics/common/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.shortlyai.analytics.common.security;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,25 +10,20 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final HeaderAuthFilter headerAuthFilter;
-
-    private final String apiPrefix;
-
-    public SecurityConfig(
-            HeaderAuthFilter headerAuthFilter,
-            @Value("${api.prefix}") String apiPrefix
-    ) {
-        this.headerAuthFilter = headerAuthFilter;
-        this.apiPrefix = apiPrefix;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.
                 csrf(AbstractHttpConfigurer::disable)
+
+                .formLogin(AbstractHttpConfigurer::disable)
+
+                .httpBasic(AbstractHttpConfigurer::disable)
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
