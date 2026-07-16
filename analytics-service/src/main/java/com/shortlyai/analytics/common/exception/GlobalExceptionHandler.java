fcad1f6980462a2f507c2359af8203a8eacb3d50
Arjun <nagarjun2790@gmail.com>
2026-07-16 17:46:53 +0530

    refractor: added exception handling for HandlerMethodValidationException for failures of validation for limit parameter and AccessDeniedException for trying to access forbidden resource

--- analytics-service/src/main/java/com/shortlyai/analytics/common/exception/GlobalExceptionHandler.java
+++ analytics-service/src/main/java/com/shortlyai/analytics/common/exception/GlobalExceptionHandler.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
+import org.springframework.security.access.AccessDeniedException;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
@@ -33,6 +35,35 @@ public class GlobalExceptionHandler {
         log.warn("Resource not found on {}: {}", request.getRequestURI(), ex.getMessage());
 
         return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
+    }
+
+    // for constraints on @RequestParam, @PathVariable
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ErrorResponse> handleHandlerMethodValidationException(
+            HandlerMethodValidationException ex,
+            HttpServletRequest request
+    ) {
+
+        String message = ex.getParameterValidationResults()
+                .getFirst()
+                .getResolvableErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        log.warn("Validation failed on {}: {}", request.getRequestURI(), message);
+
+        return buildResponse(HttpStatus.BAD_REQUEST, message, request);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(
+            AccessDeniedException ex,
+            HttpServletRequest request
+    ) {
+
+        log.warn("Resource not accessible for {}: {}", request.getRequestURI(), ex.getMessage());
+
+        return buildResponse(HttpStatus.FORBIDDEN, ex.getMessage(), request);
     }
 
     @ExceptionHandler(Exception.class)
