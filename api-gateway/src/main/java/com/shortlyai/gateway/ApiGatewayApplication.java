0b48690eac8f5a57a1618c9ecbe175bafad5f71f
Arjun <nagarjun2790@gmail.com>
2026-07-23 17:18:08 +0530

    refactor: add context-propagation dependency and enabled automatic context propagation for carrying traceId across threads

--- api-gateway/src/main/java/com/shortlyai/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/shortlyai/gateway/ApiGatewayApplication.java
@@ -3,10 +3,15 @@ package com.shortlyai.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ApiGatewayApplication {
+
+    static {
+        Hooks.enableAutomaticContextPropagation();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
